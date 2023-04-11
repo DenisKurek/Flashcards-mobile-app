8aede73741a5ce08f46d3a3f772e5bfd066ce921
DenisKurek <denis4kurek@gmail.com>
2023-04-11 13:20:14 +0200

    Contecting front-end to backend api

--- front-end/App.js
+++ front-end/App.js
@@ -12,28 +12,31 @@ import SummaryScreen from "./components/SummaryScreen";
 import AddFlashcardScreen from "./components/AddFlashcardScreen";
 import NewSetScreen from "./components/NewSetScreen";
 import AboutScreen from "./components/AboutScreen";
+import { AuthContextProvider } from "./store/AuthenticationContext.js";
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="GuestMenu">
-        <Stack.Screen name="GuestMenu" component={GuestMenu} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-        <Stack.Screen name="UserMenu" component={UserMenu} />
-        <Stack.Screen name="FlashcardsScreen" component={FlashcardsScreen} />
-        <Stack.Screen name="FlashcardScreen" component={FlashcardScreen} />
-        <Stack.Screen name="SummaryScreen" component={SummaryScreen} />
-        <Stack.Screen
-          name="AddFlashcardScreen"
-          component={AddFlashcardScreen}
-        />
-        <Stack.Screen name="NewSetScreen" component={NewSetScreen} />
-        <Stack.Screen name="AboutScreen" component={AboutScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="GuestMenu">
+          <Stack.Screen name="GuestMenu" component={GuestMenu} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="UserMenu" component={UserMenu} />
+          <Stack.Screen name="FlashcardsScreen" component={FlashcardsScreen} />
+          <Stack.Screen name="FlashcardScreen" component={FlashcardScreen} />
+          <Stack.Screen name="SummaryScreen" component={SummaryScreen} />
+          <Stack.Screen
+            name="AddFlashcardScreen"
+            component={AddFlashcardScreen}
+          />
+          <Stack.Screen name="NewSetScreen" component={NewSetScreen} />
+          <Stack.Screen name="AboutScreen" component={AboutScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContextProvider>
   );
 }
 
